362332d4dbbdd37a0da712917ffdc5db83484898
JoaoLindoo <joao.soares@ccc.ufcg.edu.br>
2018-08-01 23:10:30 -0300

    aluno login

--- src/main/java/com/appMatricula/appMatricula/AppMatriculaApplication.java
+++ src/main/java/com/appMatricula/appMatricula/AppMatriculaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
 public class AppMatriculaApplication {
 
@@ -13,8 +12,4 @@ public class AppMatriculaApplication {
 		SpringApplication.run(AppMatriculaApplication.class, args);
 	}
 	
-	@RequestMapping("/home")
-	public String index() {
-		return "index";
-	}
 }
