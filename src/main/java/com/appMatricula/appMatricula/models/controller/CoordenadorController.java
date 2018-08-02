472f297272e4bfc81cc4fbcec4077bdb8428a4e2
JoaoLindoo <joao.soares@ccc.ufcg.edu.br>
2018-08-02 17:01:58 -0300

    teste

--- src/main/java/com/appMatricula/appMatricula/models/controller/CoordenadorController.java
+++ src/main/java/com/appMatricula/appMatricula/models/controller/CoordenadorController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.appMatricula.appMatricula.models.Coordenador;
 import com.appMatricula.appMatricula.service.CoordenadorService;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class CoordenadorController {
 	
 	@Autowired
