798ea1e9b61acf34d8eb98095e27095a5b158331
JoaoLindoo <joao.soares@ccc.ufcg.edu.br>
2018-08-02 16:58:34 -0300

    teste

--- src/main/java/com/appMatricula/appMatricula/models/controller/DisciplinaController.java
+++ src/main/java/com/appMatricula/appMatricula/models/controller/DisciplinaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,9 @@ import com.appMatricula.appMatricula.models.Disciplina;
 import com.appMatricula.appMatricula.service.DisciplinaService;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class DisciplinaController {
 	
-	@RequestMapping("/home")
-	public String index() {
-		return "index";
-	}
 	
 	@Autowired
 	private DisciplinaService disciplinaService;
